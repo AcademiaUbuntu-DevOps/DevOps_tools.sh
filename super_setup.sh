471898e417260a3c6618b434ff14807a0f081252
AcademiaUbuntu-DevOps <129893430+AcademiaUbuntu-DevOps@users.noreply.github.com>
2023-05-16 22:21:07 -0300

    has to be

--- super_setup.sh
+++ super_setup.sh
@@ -14,7 +14,7 @@ end=$'\e[0m'
 
 are_you_root(){
     if [[ $EUID -ne 0 ]]; then
-        echo "This script must be run as root" 
+        echo "This script has to be run as root" 
         exit 1
     fi
 }
